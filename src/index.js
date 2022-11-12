c7149dd2a1a57cb8e5e68f534854fe07a08098b4
Vladislav <shishkin241993@gmail.com>
2022-11-11 22:23:28 -0500

    addPost/createRef

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import state from "./redux/state";
+//importing the function from the redux,to import the function need to use {}
+
+import { addPost } from "./redux/state";
+
 //import state and use it as a props
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <App
-    posts={state.profilePage.posts}
-    dialogs={state.dialogsPage.dialogs}
-    messages={state.dialogsPage.messages}
-  />
-);
+//send a function addPost as a props
+let rerenderTree = () => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <App
+      addPost={addPost}
+      posts={state.profilePage.posts}
+      dialogs={state.dialogsPage.dialogs}
+      messages={state.dialogsPage.messages}
+    />
+  );
+};
+
+rerenderTree();
