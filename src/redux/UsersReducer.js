8aee7b733e74e4e969f3723654576315697e39d4
Vladislav <shishkin241993@gmail.com>
2023-01-23 21:30:38 -0500

    loader for usersData

--- src/redux/UsersReducer.js
+++ src/redux/UsersReducer.js
@@ -3,13 +3,15 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
 const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
 //create initialState
 let initialState = {
   users: [],
   pageSize: 5,
   totalUsersCount: 0,
-  currentPage: 2,
+  currentPage: 1,
+  isFetching: false,
 };
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -51,6 +53,12 @@ const usersReducer = (state = initialState, action) => {
         totalUsersCount: action.count,
       };
     }
+    case TOGGLE_IS_FETCHING: {
+      return {
+        ...state,
+        isFetching: action.isFetching,
+      };
+    }
     default:
       return state;
   }
@@ -59,6 +67,10 @@ export default usersReducer;
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USERS, users });
 export const unFollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const toggleIsFetchingAC = (isFetching) => ({
+  type: TOGGLE_IS_FETCHING,
+  isFetching,
+});
 export const setCurrentPageAC = (currentPage) => ({
   type: SET_CURRENT_PAGE,
   currentPage: currentPage,
